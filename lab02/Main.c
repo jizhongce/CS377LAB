b941fce92459f2551743c4092dd2c14e406be581
loushengbo <loushengbo@hotmail.com>
2016-03-02 13:15:17 -0500

    lab2

--- lab02/Main.c
+++ lab02/Main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/queue.h>
 #include <semaphore.h>
+#include <unistd.h>
 #define MAX_QEQUEST 10
 
 
@@ -20,12 +21,16 @@ Request* REQUEST_ARR[MAX_QEQUEST];
 //count for elements
 int count = 0;
 
+//count for requests
+int r_count = 0;
+
 //running time
 int curr_time = 0;
 
-sem_t mutax;
+sem_t mutex;
 sem_t empty;
 sem_t full;
+
 
 
 void *Consumer_funciton(void* input){
@@ -34,20 +39,31 @@ void *Consumer_funciton(void* input){
   while (1) {
 
       sem_wait(&full);//if full = 0(empty), wait
-      sem_wait(&mutax);//access to buffer
+      sem_wait(&mutex);//access to buffer
       Request *tmp = REQUEST_ARR[count-1];
       int length = tmp->length;
+      int total_length = length;
+      int ID = tmp->ID;
 
-      while(length> 0){
-        length--;
-        curr_time++;
-        printf("I'm Consumer %i, processing request %i, processing for next %i seconds, current time is %i\n", (int)input,tmp->ID, length, curr_time);
-      }
-      printf("---------------------------------\n");
-      printf("I'm Consumer %i, done processing request %i, processed for %i seconds, current time is %i\n", (int)input,tmp->ID, tmp->length, curr_time);
       count--;
-      sem_post(&mutax);
+      sem_post(&mutex);
       sem_post(&empty);
+
+      printf("Consumer %li: , processing request %i, processing for next %i seconds, current time is %i\n", (long)input,ID, length, curr_time);
+
+      //processing
+      while(length>0){
+          sleep(1);
+          curr_time++;
+          length--;
+      }
+
+
+
+      printf("---------------------------------\n");
+      printf("Consumer %li, Completed request ID %i, at time %i\n", (long)input,ID, curr_time);
+      printf("---------------------------------\n");
+
   }
 
 }
@@ -57,22 +73,26 @@ void* Producer_function(void* input){
 
   while (count <= MAX_QEQUEST) {
     //access to buffer
-    sem_wait(&empty);//
-    sem_wait(&mutax);
+      sem_wait(&empty);
+      sem_wait(&mutex);
 
       curr_time++;
-      int length = (rand() % 100) +1;
+      int length = (rand() % 10) +1;
       Request* tmp = REQUEST_ARR[count];
-      tmp->ID = count;
+      tmp->ID = r_count;
       tmp->length = length;
+      printf("Producer: Producing request %i, length %i seconds at time %i\n", tmp->ID,tmp->length,curr_time);
       count++;
-      printf("Producing request %i, current time is %i\n", count,curr_time);
+      r_count++;
 
-      sem_post(&mutax);
+      sem_post(&mutex);
       sem_post(&full);
 
+      printf("-----------------------------------------Producer: sleeping for %li s\n",(long)input);
+      sleep(1);
 
   }
+
 
 }
 
@@ -87,14 +107,14 @@ void Consumer(int n, pthread_t input[]){
 }
 
 
-void Producer(pthread_t input[]){
-  pthread_create(&input[0], NULL, Producer_function, NULL);
+void Producer(pthread_t input[], int sleep_length){
+  pthread_create(&input[0], NULL, Producer_function, (void*)sleep_length);
 }
 
 
 int main(int argc, char const *argv[]) {
   //define semaphores
-  sem_init(&mutax,0,1);
+  sem_init(&mutex,0,1);
   sem_init(&empty,0,MAX_QEQUEST);
   sem_init(&full,0,0);
 
@@ -111,7 +131,7 @@ int main(int argc, char const *argv[]) {
   // producer
 
 pthread_t a[1];
-Producer(a);
+Producer(a,1);
 
 
 
