2a937f351ab503db0b5896d86b6b184c17ad9184
loushengbo <loushengbo@hotmail.com>
2016-03-03 14:00:16 -0500

    lab2

--- lab02/Main.c
+++ lab02/Main.c
@@ -37,7 +37,6 @@ void *Consumer_funciton(void* input){
   //printf("I'm Consumer %i\n", );
   //
   while (1) {
-
       sem_wait(&full);//if full = 0(empty), wait
       sem_wait(&mutex);//access to buffer
       Request *tmp = REQUEST_ARR[count-1];
@@ -53,16 +52,16 @@ void *Consumer_funciton(void* input){
 
       //processing
       while(length>0){
-          sleep(1);
+          sleep(5);
           curr_time++;
           length--;
       }
 
 
 
-      printf("---------------------------------\n");
+      printf("\n\n---------------------------------\n");
       printf("Consumer %li, Completed request ID %i, at time %i\n", (long)input,ID, curr_time);
-      printf("---------------------------------\n");
+      printf("---------------------------------\n\n");
 
   }
 
@@ -73,6 +72,11 @@ void* Producer_function(void* input){
 
   while (count <= MAX_QEQUEST) {
     //access to buffer
+
+      if(sem_trywait(&empty))
+          printf("The array is full, Producer stops Producing\n");
+
+          
       sem_wait(&empty);
       sem_wait(&mutex);
 
@@ -84,12 +88,12 @@ void* Producer_function(void* input){
       printf("Producer: Producing request %i, length %i seconds at time %i\n", tmp->ID,tmp->length,curr_time);
       count++;
       r_count++;
+      printf("\n-----------------------------------------Producer: sleeping for %li s\n\n",(long)input);
+      sleep(1);
 
       sem_post(&mutex);
       sem_post(&full);
 
-      printf("-----------------------------------------Producer: sleeping for %li s\n",(long)input);
-      sleep(1);
 
   }
 
@@ -109,6 +113,13 @@ void Consumer(int n, pthread_t input[]){
 
 void Producer(pthread_t input[], int sleep_length){
   pthread_create(&input[0], NULL, Producer_function, (void*)sleep_length);
+}
+
+
+void Free(){
+  for(int i=0; i<MAX_QEQUEST;i++){
+    free(REQUEST_ARR[i]);
+  }
 }
 
 
@@ -140,6 +151,7 @@ Producer(a,1);
 
 
 
+  Free();
 
 
   return 0;
