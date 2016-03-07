4281120f274dee00692bb952f8a13e175eb0263c
luyucheng <lyc0128@gmail.com>
2016-03-06 20:47:35 -0500

    add comments for Job.java in part1

--- lab02/lab02_part1/src/lab/Job.java
+++ lab02/lab02_part1/src/lab/Job.java
@@ -1,29 +1,48 @@
 package lab;
 
 public class Job {
+
 	private int total,left,CompletionTime,arrvialTime;
+
+	//This is the Job class, which takes two parameters,
+	//total, the legnth of the job
+	//arrivalTime, the arrive time of the job
+	//attribute left is the time left till completion
+	//attribute CompletionTime is time that job finishes
 	public Job(int total,int arrvialTime){
 		this.total=total;
 		left = total;
 		this.arrvialTime = arrvialTime;
 		CompletionTime = 0;
 	}
+
+	//This method simulates the processing of a job by simply minus 
+	//the time left by one
 	public void processing(){
 		left--;
 	}
+
+	//return the total of a job
 	public int getTotal(){
 		return total;
 	}
+
+	//return the left of a job
 	public int getLeft(){
 		return left;
 	}
 	
+	//set the completionTime of a job
 	public void CompletionTime(int time){
 		this.CompletionTime = time;
 	}
+
+	//return the completionTIme of a job
 	public int getCompletionTime(){
 		return this.CompletionTime;
 	}
+
+	//return the arrivalTime of a job
 	public int getArrvialTime(){
 		return arrvialTime;
 	}
