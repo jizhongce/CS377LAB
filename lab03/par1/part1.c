ed688ee68cf2b1ad7133e59b41f57ff440ba9841
loushengbo <loushengbo@hotmail.com>
2016-04-13 13:12:34 -0400

    part1 done

--- lab03/par1/part1.c
+++ lab03/par1/part1.c
@@ -6,8 +6,29 @@
 int main(){
 
   FILE *fp = fopen("data/input.txt","r");
-  unsigned int lowest;
-  fscanf(fp,"%i",&lowest);
-  printf("%u\n",lowest);
+
+  unsigned int offset_num;
+  unsigned int page_num;
+
+  fscanf(fp,"%i",&offset_num);
+  fscanf(fp,"%i",&page_num);
+
+  unsigned int offset_mask = (1<<offset_num)-1;
+  unsigned int page_mask = ((1<<page_num)-1)<<offset_num;
+
+  unsigned int tmp;
+  int count = 1;
+  
+  while (fscanf(fp,"%i",&tmp)!=-1) {
+    unsigned int offset = tmp & offset_mask;
+    unsigned int page = tmp & page_mask;
+    printf("virtual address v%i is in page number %u and offset %u.\n", count,page,offset);
+    count++;
+  }
+
+
+
+
+  fclose(fp);
 
 }
