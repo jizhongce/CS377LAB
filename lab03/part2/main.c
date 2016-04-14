a37559cfb4ec64896b052008860feb9a37f5a6d2
loushengbo <loushengbo@hotmail.com>
2016-04-13 22:16:54 -0400

    part2

--- lab03/part2/main.c
+++ lab03/part2/main.c
@@ -1,8 +1,28 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "part2.h"
 
 
 int main(){
-  
+
+  // FILE *fp = fopen("file_system","r+b");
+  // int ch;
+  // int count = 0;
+  // while ((ch = fgetc(fp)) != EOF){
+  //   if(ch==' '||ch=='\n')
+  //     continue;
+  //   if(count == 6){
+  //     fseek(fp, -1, SEEK_CUR);
+  //     fputc('2',fp);
+  //     fputc('A',fp);
+  //     fseek(fp, 0, SEEK_CUR);
+  //     break;
+  //   }
+  //   else{
+  //     count++;
+  //   }
+  //  }
+  //   fclose(fp);
+  create("nameabcd",3);
 
 }
--- lab03/part2/part2.c
+++ lab03/part2/part2.c
@@ -8,7 +8,53 @@ void ls(){
 }
 
 int create(char name[8], int size){
-  
+
+  FILE *fp = fopen("file_system","r+b");
+  int pointer[8];
+  //write to free block list
+  int b1;
+  int b2;
+  int count = size;
+  int num=0;
+  int index=0;
+  while (1) {
+    if (count==0) {
+      break;
+    }
+    //get the byte
+    b1=fgetc(fp);
+    if(b1==' '||b1=='\n')
+      continue;
+    b2=fgetc(fp);
+
+    num++;
+
+    if(b2=='1')
+      continue;
+    else{
+      pointer[index] = num;
+      index++;
+      fseek(fp, -1, SEEK_CUR);
+      fputc('1',fp);
+      fseek(fp, 0, SEEK_CUR);
+      count--;
+      continue;
+    } 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fclose(fp);
 }
 
 int delete(char name[8]){
