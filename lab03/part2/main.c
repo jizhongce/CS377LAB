df7ea225f08185481d77643f49aab8401e58b24a
Shengbo Lou <shengbolou@umass.edu>
2016-04-19 14:18:27 -0400

    part2

--- lab03/part2/main.c
+++ lab03/part2/main.c
@@ -11,6 +11,7 @@ int main(){
   char name;
   fscanf(fp,"%s",&name);
 
+
   //create disk
   printf("%s\n",&name);
   char command[90];
@@ -18,102 +19,53 @@ int main(){
   strcat(command,&name);
   system(command);
 
-  char *type = (char*)malloc(sizeof(char));
-  char *file_name;
-  int file_size;
-  fscanf(fp, "%s", &type);
-  fscanf(fp, "%s", &file_name);
-  fscanf(fp, "%i", &file_size);
 
-  printf("%s\n",&type);
-  printf("%s\n",&file_name);
-  printf("%i\n",file_size);
+  char type[2];
 
+  while (fscanf(fp,"%s",type)!=-1) {
+      printf("%s\n", type);
+    if(strcmp(type,"C")==0){
+      char name[8];
+      int size;
+      fscanf(fp,"%s",name);
+      printf("%s\n", name);
+      fscanf(fp,"%i",&size);
+      printf("%i\n", size);
+    }
+    // else if(strcmp(type,"L")){
+    //   printf("L\n");
+    //   continue;
+    // }
+    // else if(strcmp(type,"W")){
+    //   char *name;
+    //   int size;
+
+    //   fscanf(fp,"%s",name);
+    //   fscanf(fp,"%i",&size);
+    //   printf("%s\n", name);
+    //   printf("%i\n", size);
+    //   continue;
+    // }
+    // else if(strcmp(type,"D")){
+    //   char *name;
+
+    //   fscanf(fp,"%s",name);
+    //   printf("%s\n", name);
+    //  continue;
+    // }
+    // else{
+    //   char *name;
+    //   int size;
+
+    //   fscanf(fp,"%s",name);
+    //   fscanf(fp,"%i",&size);
+    //   printf("%s\n", name);
+    //   printf("%i\n", size);
+    //   continue;
+    // }
+
+  }
 
   fclose(fp);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  char* x = (char*)malloc(8*sizeof(char));
-  for (int i = 0; i < 8; ++i)
-  {
-    x[i] = 'a';
-  }
-
-  char buf[1024];
-
-  for (int i = 0; i < 1024; ++i)
-  {
-    buf[i] = 'b';
-  }  
-
-  char buf2[1024];
-
-  for (int i = 0; i < 1024; ++i)
-  {
-    buf2[i] = 'c';
-  }
-
-  f_create(x,3);
-  f_create("zzzzzzzz",3);
-
-
-
-  f_write(x,2,buf);
-  f_write("zzzzzzzz",2,buf2);
-  // f_write(x,2,buf);
- 
-
-  ls();
-
-  f_delete("zzzzzzzz");
-
-  char xy[1024]; 
-  f_read("aaaaaaaa",7,xy);
-
-  // for (int i = 0; i < 1024; ++i)
-  // {
-  //   printf("%c\n", xy[i]);
-  // }
-
-  // ls();
-
-  // f_delete("asdfgh.c");
-  // f_delete("qwerta.c");
-
-
-  free(x);
 }
